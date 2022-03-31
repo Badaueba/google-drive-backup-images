78f0e1995b7728d9c120af021f7035202a89b60b
badaueba <alan.alalabadaueba@hotmail.com>
2022-03-31 03:36:54 -0300

    fix for some src image missing

--- src/scrap/scrap-folder.ts
+++ src/scrap/scrap-folder.ts
@@ -1,5 +1,4 @@
 import { ElementHandle, Page } from "puppeteer";
-import { screenShot } from ".";
 
 const driveUrl = String(process.env["drive_url"]);
 
@@ -7,9 +6,9 @@ export async function scrapFolder(page: Page) {
     const main = await page.waitForSelector('div[data-view-type="1"]');
     const folders = (await main?.$$("div[data-target=doc]")) || [];
 
-    let count = 3;
+    let count = 0;
     console.time("scrap routine");
-    while (count === 3) {
+    while (count < folders.length) {
         const main = await page.waitForSelector('div[data-view-type="1"]');
         const folders = (await main?.$$("div[data-target=doc]")) || [];
         const folder = folders[count];
@@ -25,59 +24,59 @@ async function scrapSubfolder(page: Page, folder: ElementHandle) {
 
     const mainSelector = 'div[data-view-type="1"]';
     const thumbSelector = "div[role=gridcell]";
+    const nodeListSelector = 'img[alt~="Exibindo"].a-b-ta-Ua';
+    try {
+        await page.waitForSelector(mainSelector);
+    } catch (err) {
+        console.warn("no files in folder");
+    }
 
-    const main = await page.waitForSelector(mainSelector);
-    await page.waitForSelector(thumbSelector, { timeout: 3000 });
+    const main = await page.$(mainSelector);
+    await page.$(thumbSelector);
+    if (!main) return;
 
     let thumbs = (await main?.$$(thumbSelector)) || [];
 
     const sources: string[] = [];
     let sourceCount = 0;
 
-    while (sourceCount <= thumbs.length - 1) {
+    while (sourceCount < thumbs.length) {
         const main = await page.waitForSelector(mainSelector);
-        await page.waitForSelector(thumbSelector);
         thumbs = (await main?.$$(thumbSelector)) || [];
 
         const thumb = thumbs[sourceCount];
 
-        console.log(
-            "LIST OF files to open",
-            `${thumbs.indexOf(thumb)}/${thumbs.length}`
+        console.info(
+            "SCRAPING SRC IMG",
+            `${thumbs.indexOf(thumb) + 1}/${thumbs.length}`
         );
 
         if (thumb) {
-            await thumb.click({ clickCount: 2 });
+            await thumb.click({ clickCount: 2, delay: 500 });
         }
 
-        await page.waitFor(1000);
-        const nodeListSelector = 'img[alt~="Exibindo"].a-b-ta-Ua';
-        const openFiles = await page.$$(nodeListSelector);
+        await page.waitFor(1600);
 
-        for (const el of openFiles) {
-            const srcProperty = await el.getProperty("src");
-            const src: string = await srcProperty.jsonValue();
-            if (sources.indexOf(src) === -1) sources.push(src);
-        }
+        const alternativeList = await page.$$eval(nodeListSelector, (files) => {
+            const srcList = files.map((file) => (file as HTMLImageElement).src);
+            return srcList;
+        });
+
+        const filtered = alternativeList.filter(
+            (src) => sources.indexOf(src) === -1
+        );
+        sources.push(...filtered);
 
         await page.keyboard.press("Escape", {
-            delay: 1000,
+            delay: 100,
         });
 
         sourceCount++;
     }
 
-    console.log(sources);
+    console.info(
+        "------------------------------FINISHED-------------------------------"
+    );
+    console.info("IMG SOURCES:", sources);
     await page.goto(driveUrl);
 }
-
-// nodeListSelector = document.querySelectorAll('img[alt~="Exibindo"].a-b-ta-Ua');
-// openFiles = Array.from(nodeListSelector)
-// var sources = []
-// for (var el of openFiles) {
-//     var src = el.src;
-//      if (sources.indexOf(el) === -1) {
-//          sources.push(src);
-//          console.log(el);
-//      }
-// }
